2d45ce1ce2b1fdf3d451f20ff9263dd47d7e7a2c
fxcebx <fx.cebx@gmx.at>
2014-11-01 19:50:43 +0100

    plot all_states

--- usrunner.R
+++ usrunner.R
@@ -5,6 +5,9 @@
 # http://www.runnersworld.com/general-interest/how-does-your-state-stack-up 
 
 library(ggplot2)
+library(ggmap)
+library(maps)
+
 
 #### Data ---------------------------------------------------------------------
 # http://rw.runnersworld.com/clean/states.html -> in .xls, .csv
@@ -17,3 +20,38 @@ qplot(State,Pace.per.Mile, data= usrunner)
 qplot(Miles.per.Run, Pace.per.Mile, data= usrunner)
 qplot(Runs.per.Person, Pace.per.Mile, data= usrunner)
 qplot(Miles.per.Run, Runs.per.Person, data= usrunner)
+
+#### map
+all_states <- map_data("state")
+
+#### merge
+usrunner$State <- tolower(usrunner$State)
+Total <- merge(all_states, usrunner, by.x = "region", by.y= "State")
+head(Total)
+
+#### overall rank
+p1 <- ggplot()
+p1 <- p1 + geom_polygon(data=Total, aes(x=long, y=lat, group = group, fill=Total$Overall.Rank), colour="white") 
+       + scale_fill_continuous(low = "thistle2", high = "darkred", guide="colorbar")
+
+P1 <- p1 + theme_bw()  + labs(fill = "Overall Rank", 
+                              title = "Runners World Ranking for all 50 States", x="", y="")
+P1 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border =  element_blank())
+
+#### miles per run
+p2 <- ggplot()
+p2 <- p2 + geom_polygon(data=Total, aes(x=long, y=lat, group = group, fill=Total$Miles.per.Run), colour="white") 
++ scale_fill_continuous(low = "thistle2", high = "darkred", guide="colorbar")
+
+P2 <- p2 + theme_bw()  + labs(fill = "Miles per Run", 
+                              title = "Runners World Ranking for all 50 States", x="", y="")
+P2 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border =  element_blank())
+
+#### pace per mile
+p3 <- ggplot()
+p3 <- p3 + geom_polygon(data=Total, aes(x=long, y=lat, group = group, fill=Total$Pace.per.Mile), colour="white") 
++ scale_fill_continuous(low = "thistle2", high = "darkred", guide="colorbar")
+
+P3 <- p3 + theme_bw()  + labs(fill = "Pace per Mile", 
+                              title = "Runners World Ranking for all 50 States", x="", y="")
+P3 + scale_y_continuous(breaks=c()) + scale_x_continuous(breaks=c()) + theme(panel.border =  element_blank())
